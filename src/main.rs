0e95aabb21d670172c02b9ffc63c6b7aa5977248
Alfie <afortd@yahoo.com>
2024-09-16 17:18:36 +0200

    refactor code to make it more readable and remove about page

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,35 +18,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get_root = b"GET / HTTP/1.1\r\n";
-    let get_root_html = b"GET /index.html HTTP/1.1\r\n";
-    let get_about = b"GET /about HTTP/1.1\r\n";
-    let get_about_html = b"GET /about.html HTTP/1.1\r\n";
 
-    // Serve "index.html" for both "/" and "/index.html" paths
-    if buffer.starts_with(get_root) || buffer.starts_with(get_root_html) {
-        let contents = fs::read_to_string("index.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-
-    // Serve "about.html" for both "/about" and "/about.html" paths
-    } else if buffer.starts_with(get_about) || buffer.starts_with(get_about_html) {
-        let contents = fs::read_to_string("about.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-
-    // If the requested path is not found, return a 404 error
+    let (status_line, filename) = if buffer.starts_with(get_root) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let response = format!("{}\r\n\r\n{}", status_line, contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-
-    
-
-
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}\r\n\r\n{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
